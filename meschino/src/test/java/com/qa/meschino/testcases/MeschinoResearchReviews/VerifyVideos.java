cfa97185aad90bde19f197333d4239900582353d
playfairgame <aneelbb@mail.com>
2019-02-07 20:18:41 -0500

    update video

--- meschino/src/test/java/com/qa/meschino/testcases/MeschinoResearchReviews/VerifyVideos.java
+++ meschino/src/test/java/com/qa/meschino/testcases/MeschinoResearchReviews/VerifyVideos.java
@@ -1,7 +1,10 @@
 package com.qa.meschino.testcases.MeschinoResearchReviews;
 
 import java.util.Hashtable;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
@@ -43,6 +46,17 @@ public class VerifyVideos extends BaseTest{
 	Thread.sleep(3000);
 	
 	r.clickOnVideos();
+	Thread.sleep(3000);
+	System.out.println(driver.findElements(By.xpath("//div[@class='tab-content']/div[2]/div[3]/div[1]")).size());
+	
+	WebElement a = driver.findElement(By.xpath("//div[@class='tab-content']/div[2]/div[3]/div[1]"));
+	List<WebElement> x = a.findElements(By.tagName("a"));
+	System.out.println(x.size());
+	for(int i=0;i<x.size();i++){
+		
+     System.out.println(x.get(i).getText().trim());
+		
+	}
 	
 	}
 
